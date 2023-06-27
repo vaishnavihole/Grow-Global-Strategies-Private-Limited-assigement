19915e43caece607c9ac0a6ee3bb0625155da4cc
vaishnavihole <vaishnavihole1@gmail.com>
2023-06-27 10:27:35 +0530

    feat: user model added

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema(
+    {
+        fullName: {
+            type: String,
+            require: [true, 'fullName is required']
+        },
+        email: {
+            type: String,
+            required: [true, 'email is required'],
+            unique: true,
+        },
+        mobile: {
+            type: Number,
+            require: [true, 'mobile is required']
+        },
+        password: {
+            type: String,
+            required: [true, 'password is required'],
+            minlength: [6, 'password length must be at least 6 characters'],
+        },
+    },
+    { timestamps: true }
+);
+
+const User = model('User', userSchema)
+
+export default User;
