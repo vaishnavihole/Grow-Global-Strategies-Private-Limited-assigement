1d8350d88e41a40758e8f3397b719adfe22505c7
vaishnavihole <vaishnavihole1@gmail.com>
2023-06-27 10:14:34 +0530

    feat: server basics setup added

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from  'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
+async function connectToDB() {
+    const connection = await mongoose.connect(process.env.MONGO_DB_URL)
+    if (connection) {
+        console.log('connected to mongoDB');
+    }
+}
+connectToDB();
+
+app.listen(PORT, () => {
+    console.log(`Server started lisiting on ${PORT}`);
+  })
